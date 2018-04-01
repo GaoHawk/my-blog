c05d51287af54ed2d1981b663b749a629cb5dca5
GaoHawk <897622361@qq.com>
2018-04-01 21:28:53 +0800

    second

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div class="foot">
+    <div class="container">
+      <!-- <run-time></run-time> -->
+      <p>Made
+        <i class="fa fa-heart"></i> By Gaohawk.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import runTime from '@/components/runTime.vue'
+  export default {
+    components: {
+      runTime
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+    .foot {
+      color: #888;
+      font-size: 12px;
+      visibility:hidden;
+      position: relative;
+      margin-top: 40px;
+      padding: 15px 0 10px;
+      background-color: rgb(46, 47, 47);
+      text-align: center;
+      width: 100%;
+      /* position: fixed; */
+      /* bottom: 0; */
+    }
+   @media only screen and  (max-device-width:960px) {
+    .foot {
+      visibility:visible;
+      position: relative;
+      margin-top: 40px;
+    }
+  }
+
+
+</style>
